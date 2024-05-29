8e003890f3d70d16a6c49188add4f578644a1590
Enas Khaled <enas.ah.khaled@gmail.com>
2024-05-29 07:05:30 -0700

    test

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-# create file
+fest to create file in tmp directory
 file { '/tmp/school':
-	mode	=> '',
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	contains => 'I love Puppet'
+  ensure  => present,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
